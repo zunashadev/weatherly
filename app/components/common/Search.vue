b62f445d11b7b8a7d0114ac536fb4e74a4b5ea84
Zun Asha <zunashadev@gmail.com>
2025-12-15 21:52:58 +0700

    Update

--- app/components/common/Search.vue
+++ app/components/common/Search.vue
@@ -23,7 +23,7 @@ function getWeather() {
           v-model="city"
           type="text"
           class="peer block w-full border-b-2 border-x-transparent border-b-gray-200 border-t-transparent bg-transparent py-2.5 pe-0 ps-8 focus:border-x-transparent focus:border-b-blue-500 focus:border-t-transparent focus:ring-0 disabled:pointer-events-none disabled:opacity-50 sm:py-3 sm:text-sm dark:border-b-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:border-b-neutral-600 dark:focus:ring-neutral-600"
-          placeholder="Search Country, City, or Town"
+          placeholder="Search city"
         />
         <div
           class="pointer-events-none absolute inset-y-0 start-0 flex items-center ps-2 peer-disabled:pointer-events-none peer-disabled:opacity-50"
